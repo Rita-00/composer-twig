b606c67b76eaa068a2e4e882e8959d0fa2dd9d66
Margarita <rita_sidel@mail.ru>
2021-06-19 14:52:25 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'vendor\autoload.php';
+
+Twig_Autholoader::register();
+
+$loader = new Twig_Loader_Filesistem('templates');
+$twig=new Twig_Environment($loader);
+$template=$twig->loadTemplate('index.twig');
+$title ="jgjflkgjlkfj";
+
+$menu=array(
+  array('title'=>'Маргарита','price'=>'350'),
+  array('title'=>'4 сыра','price'=>'450'),
+  array('title'=>'Грибная','price'=>'250'),
+  array('title'=>'Пепперони','price'=>'450'),
+  array('title'=>'Римская','price'=>'550'),
+);
+echo $template->render('nav.twig',array('menu'=>$menu));
+
+?>
